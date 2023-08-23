cf7790890cc4b414bd1dd6e44387c20f90d2d363
rahmatulloh19 <raxmonovrahmatulloh@gmail.com>
2023-08-23 07:02:49 +0500

    connected to local storage

--- js/main.js
+++ js/main.js
@@ -3,23 +3,18 @@ const elList = document.querySelector(".js-hero__list");
 const elWork = document.querySelector(".js-form__input");
 const elTime = document.querySelector(".js-form__input-time");
 
-const todo = [
-  {
-    id: 1,
-    title: "Buy Benanas",
-    time: `14:12`
-  },
-  {
-    id: 2,
-    title: "Buy Apple",
-    time: `11:20`
+const todo = JSON.parse(localStorage.getItem("todos") || "[]");
+localStorage.setItem("todos", JSON.stringify(todo));
+
+renderToDos(todo);
+
+function renderToDos (array) {
+  // arrayimiz bosh bo'lganda quyidagi so'z chiqib turishi uchun
+  if(array.length === 0 && elList.textContent === "") {
+    elList.innerHTML = `<li class="hero__item hero__item--start">What will we do today?</li>`;
+  } else {
+    elList.textContent = "";
   }
-]
-
-
-
-const renderToDos = array => {
-  elList.textContent = "";
   array.forEach(item => {
     const liElement = document.createElement("li");
     liElement.classList.add("hero__item");
@@ -27,7 +22,7 @@ const renderToDos = array => {
     
     const wrapperElement = document.createElement("div");
     wrapperElement.classList.add("hero__item-inner");
-    wrapperElement.innerHTML = `<time datetime="${item.time}">${item.time}</time>
+    wrapperElement.innerHTML = `<time class="hero__time" datetime="${item.time}">${item.time}</time>
     <button class="hero__item-btn buttons-styles edit" data-id=${item.id}>
     <span class="hero__item-edit hero__item--inner buttons-styles-inner" data-id=${item.id}></span>
     </button>
@@ -40,12 +35,19 @@ const renderToDos = array => {
   })
 }
 
-renderToDos(todo);
-
 const date = new Date();
 
-let hours = date.getHours();
-let minutes = date.getMinutes();
+let hours = date.getHours().toString();
+let minutes = date.getMinutes().toString();
+
+// soatlarimiz va minutlarimiz string datatypega o'tganda birinchi kelgan nolni olib tashlaydi olib tashlangan nolni qaytardik
+if(hours.length === 1) {
+  hours = `0${hours}`;
+}
+
+if(minutes.length == 1) {
+  minutes = `0${minutes}`
+}
 
 elList.addEventListener("click", evt => {
   if(evt.target.matches(".hero__item-delete") || evt.target.matches(".delete")) {
@@ -53,6 +55,7 @@ elList.addEventListener("click", evt => {
     const deleteObj = todo.findIndex(item => item.id == deleteId);
     
     todo.splice(deleteObj, 1);
+    localStorage.setItem("todos", JSON.stringify(todo))
     renderToDos(todo);
   }
   
@@ -60,33 +63,33 @@ elList.addEventListener("click", evt => {
     const editId = evt.target.dataset.id;
     const editObj = todo.find(item => item.id == editId);
     
-    const editWork = prompt("Enter what do you change");
+    const editWork = prompt("Enter what do you change", editObj.title);
     if(editWork !== null) {
       editObj.title = editWork;
       editObj.time = hours + ":" + minutes;
     }
+    localStorage.setItem("todos", JSON.stringify(todo))
     renderToDos(todo);
   }
 })
 
 elForm.addEventListener("submit", evt => {
   evt.preventDefault();
-
+  
   const elWorkValue = elWork.value.trim();
   const elTimeValue = elTime.value;
-
+  
   let newToDo;
-
-  if(elWorkValue !== "" && elTimeValue !== "") {
-    newToDo = {
-      id: todo.length ? ++todo.length : 1,
-      title: elWorkValue,
-      time: elTimeValue
-    }
-    todo.unshift(newToDo);
-    elWork.value = "";
-    elTime.value = "";
+  
+  newToDo = {
+    id: todo.length ? todo.length + 1 : 1,
+    title: elWorkValue,
+    time: elTimeValue === "" ? `${hours}:${minutes}` : elTimeValue
   }
-
+  todo.unshift(newToDo);
+  localStorage.setItem("todos", JSON.stringify(todo))
+  elWork.value = "";
+  elTime.value = "";
+  
   renderToDos(todo);
 })
